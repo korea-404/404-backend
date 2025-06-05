c89d592f9855531f8a51b792c59e9e4ae9f07f96
96paradise <cnw0806@naver.com>
2025-06-05 12:07:05 +0900

    20250605 404 back

--- src/main/java/com/example/404-back/Application.java
+++ src/main/java/com/example/404-back/Application.java
@@ -1,13 +1,13 @@
-package com.example.korea_sleepTech_PT;
+package com.example.__back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KoreaSleepTechPtApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KoreaSleepTechPtApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
